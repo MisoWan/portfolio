3ffe360cf28cb640880d23e50c17c29a17785069
MisoWan <jopd4@naver.com>
2024-08-25 20:08:35 -0400

    Observer-part2

--- src/active_menu.js
+++ src/active_menu.js
@@ -17,35 +17,52 @@ const sectionIds = [
 
 const sections = sectionIds.map((id)=> document.querySelector(id));
 const navItems = sectionIds.map((id)=> document.querySelector(`[href="${id}"]`));
-const visibleSections = sectionIds.map(()=> false);
 
-const options = {};
+const visibleSections = sectionIds.map(()=> false);
+let activeNavItem = navItems[0];
+
+const options = {
+    rootMargin : '-20% 0px 0px 0px',
+    threshold : [0, 0.98],
+
+};
+
 const observer = new IntersectionObserver(observerCallback, options);
 sections.forEach(section => observer.observe(section));
 
 function observerCallback(entries){
     let selectLastOne; // flag 변수
     entries.forEach(entry => {
-        // console.log(entry.target);
+        console.log(entry.target);
         // console.log(entry.isIntersecting);
-        // console.log(entry.intersectionRatio);
+        console.log(entry.intersectionRatio);
         const index = sectionIds.indexOf(`#${entry.target.id}`);
         visibleSections[index] = entry.isIntersecting;
         selectLastOne = index === sectionIds.length -1 
         && entry.isIntersecting 
-        && entry.intersectingRatio >= 0.99;
+        && entry.intersectionRatio >= 0.95;
     })
     console.log(visibleSections);
-    console.log(selectLastOne);
+    console.log('무조건 라스트 섹션!', selectLastOne);
 
     const navIndex = selectLastOne 
     ? sectionIds.length-1 
     : findFirstInterSecting(visibleSections);
-    console.log(navIndex)
+    console.log(sectionIds[navIndex])
+    selectNavItem(navIndex);
+
 
     function findFirstInterSecting(intersections){
         const index = intersections.indexOf(true);
         return index >= 0 ? index : 0
+    }
+    
+    function selectNavItem(index){
+        const navItem = navItems[index];
+        if(!navItem) return;
+        activeNavItem.classList.remove('active');
+        activeNavItem = navItem;
+        activeNavItem.classList.add('active');
     }
     
 }
